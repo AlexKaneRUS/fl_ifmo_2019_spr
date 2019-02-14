56c8b183c8fa22a1f928a3ed90612d6798be7a5b
Ekaterina Verbitskaia <kajigor@gmail.com>
2019-02-14 19:31:17 +0300

    Second homework

--- Combinators.hs
+++ Combinators.hs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 {-# LANGUAGE ScopedTypeVariables #-}
 
 module Combinators where
@@ -25,10 +26,16 @@ insert t s = if find t s then t
       where
         replace :: Int -> [(Char, Trie)]
         replace i = take i l ++ [(x, helper (snd $ l !! i) xs)] ++ drop (i + 1) l
+=======
+module Combinators where
+
+import Prelude hiding (fail, fmap)
+>>>>>>> Second homework
 
 -- Parsing result is some payload and a suffix of the input which is yet to be parsed
 newtype Parser str ok = Parser { runParser :: str -> Maybe (str, ok) }
 
+<<<<<<< HEAD
 instance Functor (Parser str) where
   fmap f p = Parser $ \s ->
     case runParser p s of
@@ -71,12 +78,29 @@ instance Monad (Parser str) where
 -- Parser which always succeedes consuming no input
 success :: ok -> Parser str ok
 success = pure
+=======
+-- Parser which always succeedes consuming no input
+success :: ok -> Parser str ok
+success ok = Parser $ \s -> Just (s, ok)
+
+-- Parser which fails no mater the input
+fail :: Parser str ok
+fail = Parser $ const Nothing
+
+-- Biased choice: if the first parser succeedes, the second is never run
+(<|>) :: Parser str ok -> Parser str ok -> Parser str ok
+p <|> q = Parser $ \s ->
+  case runParser p s of
+    Nothing -> runParser q s
+    x -> x
+>>>>>>> Second homework
 
 -- Default sequence combinator
 -- If the first parser succeedes then the second parser is used
 -- If the first does not succeed then the second one is never tried
 -- The result is collected into a pair
 seq :: Parser str a -> Parser str b -> Parser str (a, b)
+<<<<<<< HEAD
 p `seq` q = Parser $ \s ->
   case runParser p s of
     Just (str, ok) -> runParser (fmap ((,) ok) q) str
@@ -101,10 +125,48 @@ satisfy p = Parser $ \s ->
   case s of
     (t' : s') | p t' -> Just (s', t')
     _         -> Nothing
+=======
+p `seq` q = undefined
+
+-- Monadic sequence combinator
+(>>=) :: Parser str a -> (a -> Parser str b) -> Parser str b
+p >>= q = undefined
+
+-- Applicative sequence combinator
+(<*>) :: Parser str (a -> b) -> Parser str a -> Parser str b
+p <*> q = undefined
+
+-- Applies a function to the parsing result, if parser succeedes
+fmap :: (a -> b) -> Parser str a -> Parser str b
+fmap f p = Parser $ \s ->
+  case runParser p s of
+    Just (s', a) -> Just (s', f a)
+    _ -> Nothing
+
+-- Applies a parser once or more times
+some :: Parser str a -> Parser str [a]
+some p = undefined
+
+-- Applies a parser zero or more times
+many :: Parser str a -> Parser str [a]
+many p = undefined
+
+-- Parses keywords 
+keywords :: [String] -> Parser String String
+keywords kws = undefined
+
+-- Checks if the first element of the input is the given token
+token :: Eq token => token -> Parser [token] token
+token t = Parser $ \s ->
+  case s of
+    (t' : s') | t == t' -> Just (s', t)
+    _ -> Nothing
+>>>>>>> Second homework
 
 -- Checks if the first character of the string is the one given
 char :: Char -> Parser String Char
 char = token
+<<<<<<< HEAD
 
 space :: Parser String Char
 space = satisfy isSpace
@@ -123,3 +185,5 @@ eof = Parser $ \str -> if null str then Just ([], ()) else Nothing
 
 getInput :: Parser String String
 getInput = Parser $ \str -> Just (str, str)
+=======
+>>>>>>> Second homework
